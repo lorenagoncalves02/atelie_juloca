404af7f60023bfd37dedef25ed38d978e8232172
julia cereda <julia.cereda@aluno.senai.br>
2026-06-11 17:30:46 -0300

    add: finalizando

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+from model.produtos import select_produtos
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def pg_index():
+    return render_template ('index.html')
+
+@app.route("/produtos")
+def pg_produtos():
+    itens_produtos = select_produtos()
+    return render_template ("produto.html", itens_produtos = itens_produtos)
+
+#Iniciando o servidor Flask
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080", debug=True)
